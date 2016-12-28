a3a0ce0a8924f7ed1260ab1ba75d654dcbf6be6c
Budi Prakosa <iyok@deadmediafm.org>
2016-12-28 09:56:46 +0700

    enable Z safe homing, and match XY probe speed with tronxy config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -586,7 +586,7 @@
 #define Z_PROBE_OFFSET_FROM_EXTRUDER -0.5   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 8000
+#define XY_PROBE_SPEED 6000
 // Speed for the first approach when double-probing (with PROBE_DOUBLE_TOUCH)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
 // Speed for the "accurate" probe of each point
@@ -880,7 +880,8 @@
 // - If stepper drivers time out, it will need X and Y homing again before Z homing.
 // - Move the Z probe (or nozzle) to a defined XY point before Z Homing when homing all axes (G28).
 // - Prevent Z homing when the Z probe is outside bed area.
-//#define Z_SAFE_HOMING
+
+#define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
   #define Z_SAFE_HOMING_X_POINT ((max(X_MIN_POS, 0) + X_MAX_POS) / 2)    // X point for Z homing when homing all axis (G28).
